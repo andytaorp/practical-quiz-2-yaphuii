0c8d2070e09b80ffd9eaa5987c206267d0f8e1b5
yaphuii <23004055@myrp.edu.sg>
2024-12-03 22:32:33 +0800

    YapHui_FavouriteMovieListApp

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,35 @@ function App() {
 
   const handleAddMovie = (title) => {
     // TODO: Implement adding a new movie to the list
+    const newMovie = { id: Date.now(), title, watched: false }; // Create new movie object
+    setMovies([...movies, newMovie]); // Add movie to the list
   };
 
   const handleToggleWatched = (id) => {
     // TODO: Implement toggling a movie's watched status
+    setMovies(
+      movies.map((movie) =>
+        movie.id === id ? { ...movie, watched: !movie.watched } : movie
+      )
+    );
   };
 
   const handleDeleteMovie = (id) => {
     // TODO: Implement deleting a movie from the list
+    setMovies(movies.filter((movie) => movie.id !== id)); // Remove movie from list
   };
 
   return (
     <div>
       <h1>Favorite Movies</h1>
       {/* TODO: Add AddMovieForm Component */}
+      <AddMovieForm onAddMovie={handleAddMovie} />
       {/* TODO: Add MovieList Component */}
+      <MovieList
+        movies={movies}
+        onToggleWatched={handleToggleWatched}
+        onDeleteMovie={handleDeleteMovie}
+      />
     </div>
   );
 }
